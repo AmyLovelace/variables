fe9ecaf9fee3c8a34f247b00272fd4cc2064e9ed
amanecer <amanecer.luz.cabrera@gmail.com>
2023-02-25 19:05:17 -0300

    added a html and js file

---
+++ storage.js
@@ -0,0 +1,17 @@
+//localstorage
+        //metodo1
+        /*localStorage.nombre="Amanecer luz";
+
+        //metodo2
+        localStorage.setItem("nombre","Amanecer luz");
+
+        //muestra en pantalla
+        document.write(localStorage.nombre);
+
+        //eliminar elemento
+        localStorage.removeItem("nombre");*/
+
+        //session storage
+
+        
+        document.write(sessionStorage.nombre)
